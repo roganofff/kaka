59ef6dcd7bc97d8331b2f054d787f6f98f68119d
roganofff <matematic1337@gmail.com>
2023-02-05 12:13:43 +0300

    script

---
+++ abc.py
@@ -0,0 +1,13 @@
+num = int(input("Enter a num of value: "))
+ternary_num = list()
+alphabet = {0: "A", 1: "O", 2: "Y"}
+
+rem = num - 1
+while rem > 0:
+    ternary_num.append(rem % 3)
+    rem //= 3
+
+for i in range(0, len(ternary_num)):
+    ternary_num[i] = alphabet[ternary_num[i]]
+
+print('A' * (5 - len(ternary_num)) + ''.join(reversed(ternary_num)))
